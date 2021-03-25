c0837d93b0bbd00d96c41b329c37f1c600c4bcfe
miu oohira <miu.000408@gmail.com>
2021-03-25 23:04:08 +0900

    ユーザモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_091810) do
+ActiveRecord::Schema.define(version: 2021_03_25_135537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2021_03_19_091810) do
     t.datetime "deadline", null: false
     t.integer "state", default: 0, null: false
     t.integer "priority", default: 0, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
