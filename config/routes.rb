be813b4a9319db2fe1103b2a0774974145c02c4b
miu oohira <miu.000408@gmail.com>
2021-03-26 00:05:55 +0900

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :tasks
   root 'tasks#index'
   resources :users, only: [:new, :create, :show]
-
+  resources :sessions, only: [:new, :create, :destroy]
 end
