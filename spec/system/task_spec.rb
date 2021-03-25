68b156f27a9b86deb60f0efc853243f67cbc603f
miu oohira <miu.000408@gmail.com>
2021-03-25 11:45:24 +0900

    テスト（system）修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -7,74 +7,74 @@ RSpec.describe 'タスク管理機能', type: :system do
   #   visit tasks_path
   # end
   
-  # describe '新規作成機能' do
-  #   context 'タスクを新規作成した場合' do
-  #     it '作成したタスクが表示される' do
-  #       visit new_task_path
-  #       fill_in 'タイトル', with: 'test_title'
-  #       fill_in '内容', with: 'test_content'
-  #       fill_in :task_deadline, with: '002022-03-18-15:00'
-  #       select '未着手', from: :task_state
-  #       select '低', from: :task_priority
-  #       click_button '登録する'
-  #       expect(page).to have_content 'test_title'
-  #       expect(page).to have_content 'test_content'
-  #       expect(page).to have_content '2022-03-18 15:00'
-  #       expect(page).to have_content '未着手'
-  #       expect(page).to have_content '低'
-  #     end
-  #   end
-  # end
+  describe '新規作成機能' do
+    context 'タスクを新規作成した場合' do
+      it '作成したタスクが表示される' do
+        visit new_task_path
+        fill_in 'タイトル', with: 'test_title'
+        fill_in '内容', with: 'test_content'
+        fill_in :task_deadline, with: '002022-03-18-15:00'
+        select '未着手', from: :task_state
+        select '低', from: :task_priority
+        click_button '登録する'
+        expect(page).to have_content 'test_title'
+        expect(page).to have_content 'test_content'
+        expect(page).to have_content '2022-03-18 15:00'
+        expect(page).to have_content '未着手'
+        expect(page).to have_content '低'
+      end
+    end
+  end
 
-  #describe '一覧表示機能' do
-    # context '一覧画面に遷移した場合' do
-    #   it '作成済みのタスク一覧が表示される' do
-    #     # task = FactoryBot.create(:task, title: 'task')
-    #     visit tasks_path
-    #     expect(page).to have_content 'task'
-    #   end
-    # end
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのタスク一覧が表示される' do
+        # task = FactoryBot.create(:task, title: 'task')
+        visit tasks_path
+        expect(page).to have_content 'task'
+      end
+    end
 
-    # context 'タスクが作成日時の降順に並んでいる場合' do
-    #   it '新しいタスクが一番上に表示される' do
-    #     # task = FactoryBot.create(:task, title: 'task')
-    #     task = FactoryBot.create(:second_task, title: 'task')
-    #     visit tasks_path
-    #     task_list = all('.task_row') 
-    #     expect(task_list[0]).to have_content 'test_content2'
-    #     expect(task_list[1]).to have_content 'test_content'
-    #   end
-    # end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        # task = FactoryBot.create(:task, title: 'task')
+        task = FactoryBot.create(:second_task, title: 'task')
+        visit tasks_path
+        task_list = all('.task_row') 
+        expect(task_list[0]).to have_content 'test_content2'
+        expect(task_list[1]).to have_content 'test_content'
+      end
+    end
 
-    # context 'タスクが終了期限の降順に並んでいる場合' do
-    #   it '新しいタスクが一番上に表示される' do
-    #     # task = FactoryBot.create(:task, title: 'task')
-    #     # task = FactoryBot.create(:second_task, title: 'task')
-    #     # visit new_task_path
-    #     # fill_in 'タイトル', with: 'test_title'
-    #     # fill_in '内容', with: 'test_content'
-    #     # fill_in :task_deadline, with: '002021-03-19-15:00'
-    #     # click_button '登録する'
-    #     visit tasks_path
-    #     click_link '終了期限でソートする'
-    #     sort_expired = all('.sort_expired_row')
-    #     expect(sort_expired[0]).to have_content '2022-03-18 15:00'
-    #     expect(sort_expired[1]).to have_content '2022-03-19 15:00'
-    #     expect(sort_expired[2]).to have_content '2022-03-20 15:00'
-    #   end
-    # end
-  #end
+    context 'タスクが終了期限の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        # task = FactoryBot.create(:task, title: 'task')
+        # task = FactoryBot.create(:second_task, title: 'task')
+        # visit new_task_path
+        # fill_in 'タイトル', with: 'test_title'
+        # fill_in '内容', with: 'test_content'
+        # fill_in :task_deadline, with: '002021-03-19-15:00'
+        # click_button '登録する'
+        visit tasks_path
+        click_link '終了期限でソートする'
+        sort_expired = all('.sort_expired_row')
 
-#   describe '詳細表示機能' do
-#     context '任意のタスク詳細画面に遷移した場合' do
-#       it '該当タスクの内容が表示される' do
-#         # task = FactoryBot.create(:task, title: 'task')
-#         visit tasks_path
-#         click_link '詳細を確認する'
-#         expect(page).to have_content 'task'
-#       end
-#     end
-#  end
+        expect(sort_expired[0]).to have_content '2022-03-18 15:00'
+        expect(sort_expired[1]).to have_content '2022-03-19 15:00'
+        expect(sort_expired[2]).to have_content '2022-03-20 15:00'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context '任意のタスク詳細画面に遷移した場合' do
+      it '該当タスクの内容が表示される' do
+        # task = FactoryBot.create(:task, title: 'task')
+        visit tasks_path
+        all('tbody tr')[0].click_link '詳細'
+      end
+    end
+ end
 
   describe '検索機能' do
     context 'タイトルであいまい検索をした場合' do
