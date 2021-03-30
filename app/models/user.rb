a1d3a53c6bfcb3012d4668636bc201f66af96106
miu oohira <miu.000408@gmail.com>
2021-03-30 15:50:47 +0900

    管理ユーザーが０にならないよう制御

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_destroy :destroy_admin
   has_many :tasks, dependent: :destroy
   validates :name,  presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
@@ -7,4 +8,12 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  private
+  def destroy_admin
+    return unless self.admin?
+    
+    if User.where(admin: true).count <= 1
+      throw(:abort)
+    end
+  end
 end
