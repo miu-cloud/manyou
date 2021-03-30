6a04774de9f2441504c0c1e192e0f12e0adc4f31
miu oohira <miu.000408@gmail.com>
2021-03-30 23:58:08 +0900

    FactoryBotを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
     name { "一般ユーザー" }
-    sequence(:email) { "user@test.com" }
-    password { "user_password" }
+    sequence(:email) { "user@example.jp" }
+    password { "111111" }
     admin { false }
   end
   factory :admin, class: User do
