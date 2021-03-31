8f663d3c8e407ec18caeea57601e7cfbfc9a0669
miu oohira <miu.000408@gmail.com>
2021-04-01 00:07:48 +0900

    task(システムテスト)修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -12,9 +12,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     fill_in 'session[password]', with: user.password
     click_button 'Log in'
   end
-  # before do
-  #   visit tasks_path
-  # end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -38,7 +36,6 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        # task = FactoryBot.create(:task, title: 'task')
         login(user)
         visit tasks_path
         expect(page).to have_content 'task'
@@ -47,28 +44,20 @@ RSpec.describe 'タスク管理機能', type: :system do
 
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        # task = FactoryBot.create(:task, title: 'task')
-        task = FactoryBot.create(:second_task, title: 'task')
+        login(user)
         visit tasks_path
         task_list = all('.task_row') 
-        expect(task_list[0]).to have_content 'test_content2'
-        expect(task_list[1]).to have_content 'test_content'
+        expect(task_list[0]).to have_content 'test_content3'
+        expect(task_list[1]).to have_content 'test_content2'
       end
     end
 
     context 'タスクが終了期限の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        # task = FactoryBot.create(:task, title: 'task')
-        # task = FactoryBot.create(:second_task, title: 'task')
-        # visit new_task_path
-        # fill_in 'タイトル', with: 'test_title'
-        # fill_in '内容', with: 'test_content'
-        # fill_in :task_deadline, with: '002021-03-19-15:00'
-        # click_button '登録する'
+        login(user)
         visit tasks_path
         click_link '終了期限でソートする'
         sort_expired = all('.sort_expired_row')
-
         expect(sort_expired[0]).to have_content '2022-03-18 15:00'
         expect(sort_expired[1]).to have_content '2022-03-19 15:00'
         expect(sort_expired[2]).to have_content '2022-03-20 15:00'
@@ -79,7 +68,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        # task = FactoryBot.create(:task, title: 'task')
+        login(user)
         visit tasks_path
         all('tbody tr')[3].click_link '詳細'
       end
@@ -89,6 +78,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '検索機能' do
     context 'タイトルであいまい検索をした場合' do
       it '検索キーワードを含むタスクで絞り込まれる' do
+        login(user)
         visit tasks_path
         fill_in 'title', with: 'title2'
         click_button 'Search'
@@ -97,6 +87,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'ステータス検索をした場合' do
       it "ステータスに完全一致するタスクが絞り込まれる" do
+        login(user)
         visit tasks_path
         select '未着手', from: :state
         click_button 'Search'
@@ -105,6 +96,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タイトルのあいまい検索とステータス検索をした場合' do
       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
+        login(user)
         visit tasks_path
         fill_in 'title', with: 'title2'
         select '着手中', from: :state
