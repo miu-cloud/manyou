b241db79ec647d55f0e2e8d5508e4a2deef4000b
miu oohira <miu.000408@gmail.com>
2021-04-02 11:01:01 +0900

    シードデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,27 @@
 
-# User.create!(name:  "管理者",
-#     email: "admin@example.jp",
-#     password:  "000000",
-#     password_confirmation: "000000",
-#     admin: true)
+User.create!(name:  "管理者",
+    email: "admin@example.jp",
+    password:  "000000",
+    password_confirmation: "000000",
+    admin: true)
 
-# 10.times do |n|
-#     User.create!(
-#         email: "test#{n + 1}@test.com",
-#         name: "テスト太郎#{n + 1}",
-#         password: 000000,
-#         password_confirmation: 000000
-#     )
-# end
+10.times do |n|
+    User.create!(
+        email: "test#{n + 1}@test.com",
+        name: "テスト太郎#{n + 1}",
+        password: 000000,
+        password_confirmation: 000000
+    )
+end
 
-# 10.times do |n|
-#     Task.create!(
-#         title: "test#{n + 1}",
-#         content: "テスト太郎#{n + 1}",
-#         user_id: user.first.id + n
-#     )
-# end
+10.times do |n|
+    Task.create!(
+        title: "test#{n + 1}",
+        content: "テスト太郎#{n + 1}",
+        deadline: "002022-04-01#{n + 1}-15:00"
+        user_id: user.(first.id + n)
+    )
+end
 
 10.times do |n|
     Label.create!(
